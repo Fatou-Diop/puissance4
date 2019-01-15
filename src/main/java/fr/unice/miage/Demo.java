50192f076d165d64c69c8a424f75f496806c699b
Fatou Diop <kine.fd@gmail.com>
2019-01-15 14:51:34 +0100

    correction des erreurs sur les branches

---
+++ src/main/java/fr/unice/miage/Demo.java
@@ -0,0 +1,57 @@
+package fr.unice.miage;
+import fr.unice.miage.*;
+
+public class Demo implements IDemo{
+
+	
+	 public final static int vide = 0;
+	 private int[][] grille;  
+	 private int taille;
+	   public boolean estPlein() {
+	      for (int col = 0; col < taille; col++) {
+	        for (int ligne = 0; ligne < taille; ligne++) {
+	          if (grille[col][ligne] == vide) {
+	            return false;
+	          }
+	        }
+	      }
+
+	      return true;
+	    }
+
+	    public int getTaille() {
+	      return taille;
+	    }
+
+	    public void afficher() {
+	      for (int ligne = taille - 1; ligne >= 0; --ligne) {
+	        for (int col = 0; col < taille; col++) {
+	          switch (grille[col][ligne]) {
+	          case vide:
+	            System.out.print(' ');
+	            break;
+	          case 'x':
+	            System.out.print('x');
+	            break;
+	          case 'o':
+	            System.out.print('o');
+	            break;
+	          }
+	        }
+	        System.out.println();
+	      }
+
+	      for (int i = 0; i < taille; ++i) {
+	        System.out.print('-');
+	      }
+	      System.out.println();
+	      for (int i = 1; i <= taille; ++i) {
+	        System.out.print(i);
+	      }
+	      System.out.println();
+	    }
+	  
+	
+
+}
+
---
+++ src/main/java/fr/unice/miage/IBoard.java
@@ -0,0 +1,5 @@
+package fr.unice.miage;
+
+public interface IBoard {
+
+}
