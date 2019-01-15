1128e11506ed7932f73bf3927db88997293375c4
Fatou Diop <kine.fd@gmail.com>
2019-01-15 11:32:45 +0100

    Implémentation de la classe démo et quelques modification sur la classe Partie.java

--- src/main/java/fr/unice/miage/Partie.java
+++ src/main/java/fr/unice/miage/Partie.java
@@ -1,6 +1,24 @@
 package fr.unice.miage;
 
 public class Partie {
-   private Joueur j1;
-   private Joueur j2;
+	private Joueur[] joueurs = new Joueur[2];
+   private Demo demo;
+  
+   public Partie (Joueur joueur1, Joueur joueur2) {
+   demo = new Demo();
+   joueurs[0] = joueur1;
+   joueurs[1] = joueur2;
+   
+   }
+   
+   
+   
+   public void demo() {
+	   int vainqueur = 1;
+	   int cJoueur = 0;
+	  
+	   
+   }
 }
+   
+   
